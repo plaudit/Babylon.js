0ae3dd4989c76d5cf31341ec0d8735d7212eaa18
Michael Schlotfeldt <michael@plaudit.com>
2017-11-12 17:14:47 -0500

    When serializing the isEnabled should reflect the state of the mesh itself and not the ancestor's state so when deserialized version will be the same.

--- src/Mesh/babylon.mesh.ts
+++ src/Mesh/babylon.mesh.ts
@@ -2045,7 +2045,7 @@
             serializationObject.scaling = this.scaling.asArray();
             serializationObject.localMatrix = this.getPivotMatrix().asArray();
 
-            serializationObject.isEnabled = this.isEnabled();
+            serializationObject.isEnabled = this.isEnabled(false);
             serializationObject.isVisible = this.isVisible;
             serializationObject.infiniteDistance = this.infiniteDistance;
             serializationObject.pickable = this.isPickable;
